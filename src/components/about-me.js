40ebaaf03f10e9465266aee37ea4689dbfd9b6a5
Joshua-Aguilar <joshuaaguilar20@gmail.com>
2018-09-13 16:15:20 -0700

    hello

--- src/components/about-me.js
+++ src/components/about-me.js
@@ -20,11 +20,11 @@ const AboutMe = ({ profileImg, hobbyImgs }) => {
           <div className="col-12 col-sm-12 col-md-6">
             <h3>My Current Plans</h3>
             <p>
-            My goal as web developer is to help build applications that assist humanity. I want to help create more access to simple yet powerful code. I have always had a unique fascination with coding, and computer science. After 5 years in the medical field, I decided to chase my true passion of computer science. I quit my day job, and started studying computer science daily. I could not afford a boot-camp, so I spend every  day for 8 months coding, and finding mentors. I started by building every FreeCodeCamp project while simultaneously taking computer science, and algorithm courses . 
+            My goal as web developer is to help build applications that assist humanity. I want to help create more access to simple yet powerful code. I have always had a unique fascination with coding. After 5 years in the Emergency Department  I decided to chase my true passion of computer science. I quit my day job and started studying computer science daily. I could not afford a boot-camp so I spend every  day for 8 months coding, and finding mentors. I started by building every FreeCodeCamp project while simultaneously taking computer science, and algorithm courses . 
             After building over 20 projects with guidance from a mentor I started contributing to open source projects. 
-            I then got involved with the FCC community as a moderator, and mentor. 
-            Shortly after I started taking projects for clients, and helping tutor students who are new to CS.
-            After hearing feedback from other students I started an algorithms, and data structures meet-up in northern California.   
+            I then got involved with the FCC community as a moderator and mentor. 
+            Shortly after I started taking projects for clients and helping tutor students who are new to CS.
+            After hearing feedback from other students I started an algorithms and data structures meet-up in northern California.   
             </p>
             <h4>Project's I am building</h4>
             <p>
@@ -35,7 +35,7 @@ const AboutMe = ({ profileImg, hobbyImgs }) => {
                 FreeCodeCamp,
               </a>{" "}
               and help tutor new indivials in Javascript. I am currently assisting with the babel
-              open source project, and a healthcare application in my spare time. I am also working on building 
+              open source project and a healthcare application in my spare time. I am also working on building 
               applications for the GitHub Hackaton October 26th presented by FCC.  
               
               
